00935a095363c93637724fb091103ce27f85e74b
Fabian Varela Bonett <fvarela@smarttaxi.co>
2019-05-27 16:09:42 -0500

    Fix run database in app

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
-from db import db
-
 app = Flask(__name__)
 app.secret_key = "fabian"
 
@@ -21,10 +19,6 @@ app.config["JWT_AUTH_HEADER_PREFIX"] = "Bearer" # Change prefix default JWT
 #SQL Alchemy Configurations
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 api = Api(app)
 jwt = JWT(app, authenticate, identity)
@@ -50,7 +44,7 @@ api.add_resource(Item, "/item/<string:name>")
 api.add_resource(ItemList, "/items")
 api.add_resource(UserRegister, "/register")
 
-db.init_app(app)
-
 if __name__ == "__main__":
+    from db import db
+    db.init_app(app)
     app.run(port=5000, debug=True)
