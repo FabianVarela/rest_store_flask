1c0b6d774924e1fe1e2bc0f8034afa6f79b1dbeb
Fabian Varela Bonett <jfabian.303@gmail.com>
2019-06-08 13:11:46 -0500

    Custom JWT callbacks and responses

--- app.py
+++ app.py
@@ -32,12 +32,53 @@ api = swagger.docs(Api(app), apiVersion='0.1', api_spec_url='/api/spec', descrip
 #JWT
 jwt = JWTManager(app)
 
+# Method to manage claims and permissions
 @jwt.user_claims_loader
 def add_claims_to_jwt(identity):
-    if identity == 1: # Cane be replaced by other configuration
+    if identity == 1: # Can be replaced by other configuration
         return {"is_admin": True}
     
     return {"is_admin": False}
+
+# Message when the token has expired
+@jwt.expired_token_loader
+def expired_token_callback():
+    return jsonify({
+        "description": "The token has been expired",
+        "error": "token_expired"
+    }), 401
+
+# Message when the token is invalid
+@jwt.invalid_token_loader
+def invalid_token_callback(error):
+    return jsonify({
+        "description": "Signature verification failed",
+        "error": "invalid_token"
+    }), 401
+
+# Message when the request does not send the token
+@jwt.unathorized_loader
+def missing_token_callback(error):
+    return jsonify({
+        "description": "Request doesn't containt an access token",
+        "error": "authorization_required"
+    }), 401
+
+# Message when the token is not fresh
+@jwt.needs_fresh_token_loader
+def token_not_refresh_callback():
+    return jsonify({
+        "description": "The token is not fresh",
+        "error": "fresh_token_required"
+    }), 401
+
+# Message when the token has expired and has been revoked
+@jwt.revoked_token_loader
+def revoked_token_callback():
+    return jsonify({
+        "description": "The token has been revoked",
+        "error": "token_revoked"
+    }), 401
 
 # Add resources or routes
 api.add_resource(Store, "/store/<string:name>")
